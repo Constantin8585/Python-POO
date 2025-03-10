5fe8192dc9e442e074cc152065f23ed39ede5581
Constantin8585 <gadegbeabednego@gmail.com>
2025-03-10 13:59:11 +0000

    ajout de la classe PersonneMorale pour la gestion des informations de l'entité

---
+++ Certificat/Personne_Morale.py
@@ -0,0 +1,10 @@
+from Certificat.Personne import Personne
+
+
+class PersonneMorale(Personne):
+
+    def __init__(self, Tocomplete):
+        self.ToComplete = Tocomplete
+
+    def Afficher_RS(self):
+        return self.ToComplete
