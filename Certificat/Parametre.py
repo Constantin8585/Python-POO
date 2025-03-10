2547d72b894e0d254b7b5243a4be4f4f16742410
Constantin8585 <gadegbeabednego@gmail.com>
2025-03-10 14:11:22 +0000

    ajout de la classe Parametre pour la gestion des paramètres de certificat

---
+++ Certificat/Parametre.py
@@ -0,0 +1,8 @@
+class Parametre:
+
+    def __int__(self,code_par, lib_par, nb_par, type_par):
+        self.code_par = code_par
+        self.lib_par = lib_par
+        self.nb_par = nb_par
+        self.type_par = type_par
+      
